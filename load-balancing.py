9c4730dc9377d30e321ddd5b65b2608ca79cae17
Tamás Kis <kis.tamas@sztaki.hu>
2023-05-23 20:48:27 +0000

    load balancing application

---
+++ load-balancing.py
@@ -0,0 +1,22 @@
+import dimod
+from dwave.system import LeapHybridSampler
+
+def build_model(jobs : list, T : int):
+    cqm = dimod.ConstrainedQuadraticModel()
+    n = len(jobs)
+    x = [[dimod.Binary(f'x_{i}_{j}') for j in range(T)] for i in range(n)]
+    i = 0
+    for [r,d] in jobs:
+        cqm.add_constraint(sum([x[i][j] for j in range(r, d+1)] == 1 ))
+        for j in range(T):
+            if j < r or j > d:
+                cqm.set_upper_bound(x[i][j],0)
+        i=i+1
+    cqm.set_objective(sum([sum([x[i][j] for i in range(n)]) * sum([sum([x[i][j] for i in range(n)]) for j in range(T) ])]))
+
+    return cqm
+
+job_lst = [[0,2],[1,3],[1,2],[2,3],[1,2]]
+sampler = LeapHybridSampler()
+cqm = build_model(job_lst,4)
+results = sampler.sample(cqm, label='load balancing')
