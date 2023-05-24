59cdef8f11388b6326959473b5f2e6cd0ef2e7e2
Tamás Kis <kis.tamas@sztaki.hu>
2023-05-24 09:15:59 +0000

    jav

--- load-balancing.py
+++ load-balancing.py
@@ -4,20 +4,30 @@ from dwave.system import LeapHybridCQMSampler
 def build_model(jobs : list, T : int):
     cqm = dimod.ConstrainedQuadraticModel()
     n = len(jobs)
-    x = [[dimod.Binary(f'x_{i}_{j}') for j in range(T)] for i in range(n)]
+    x = [[dimod.Binary(f'x_{i}_{j}') for j in range(T)] for i in range(n) ]
     i = 0
     for r,d in jobs:
         cqm.add_constraint(sum([x[i][j] for j in range(r, d+1)]) == 1 )
-        for j in range(T):
-            if j < r or j > d:
-                cqm.add_constraint(x[i][j]==0)
         i=i+1
-    cqm.set_objective(sum([sum([x[i][j] for i in range(n)]) * sum([sum([x[i][j] for i in range(n)]) for j in range(T) ])]))
+    cqm.set_objective(sum([sum([x[i][j] for i in range(n) if j >= jobs[i][0] and j<=jobs[i][1]]) **2 for j in range(T) ]))
 
     return cqm
 
 job_lst = [[0,2],[1,3],[1,2],[2,3],[1,2]]
-sampler = LeapHybridCQMSampler()
+
+
 cqm = build_model(job_lst,4)
-results = sampler.sample_cqm(cqm, time_limit=180, label='load balancing')
-print( results)
+
+sampler = LeapHybridCQMSampler()
+sampleset = sampler.sample_cqm(cqm, time_limit=180, label='load balancing')
+
+feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)  
+
+if len(feasible_sampleset):      
+   best = feasible_sampleset.first
+   print("{} feasible solutions of {}.".format(
+      len(feasible_sampleset), len(sampleset)))
+   print("best: ",best) 
+   i = 0
+   for r,d in jobs:
+       print([best.sample[f'x_{i}_{j}'] for j in range(r,d+1)])
